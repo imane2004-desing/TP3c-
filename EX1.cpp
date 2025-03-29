ca1dfd8a3e949607453881c05144d1e408dd754a
imane elkourari <imanekourari@gmail.com>
2025-03-29 11:01:47 +0100

    Update EX1.cpp

--- EX1.cpp
+++ EX1.cpp
@@ -1,116 +1,135 @@
 #include <iostream>
-
-
-
-#include <iostream>
-#include <iomanip>
-
 using namespace std;
 
-class Ensemble {
-private:
-    int *elements;
+class Ensemble
+{
     int taille;
-    int nb_elements;
-
+    int* tab;
+    int poslibre;
 public:
-    Ensemble(int taille = 100) : taille(taille), nb_elements(0) {
-        elements = new int[taille];
-    }
+    Ensemble(int p)
+    {
+        tab=new int [p];
+        taille=p;
+        poslibre=0;
 
-    ~Ensemble() {
-        delete[] elements;
     }
-
-    void empty() {
-        nb_elements = 0;
+    bool isEmpty()
+    {
+        return  poslibre == 0;
     }
-
-    void add(int element) {
-        if (nb_elements < taille) {
-            // Vérifier si l'élément existe déjà
-            if (!is_member(element)) {
-                elements[nb_elements++] = element;
-            } else {
-                cout << "L'élément " << element << " est déjà présent dans l'ensemble." << endl;
-            }
-        } else {
-            cout << "L'ensemble est plein. Impossible d'ajouter " << element << "." << endl;
+    void add(int element)
+    {
+        if ( poslibre<taille)
+        {
+            tab[poslibre] = element;
+            poslibre++;
+        }
+        else
+        {
+            cout << "Ensemble plein !" << endl;
         }
     }
-
-    void remove(int element) {
-        for (int i = 0; i < nb_elements; ++i) {
-            if (elements[i] == element) {
-                elements[i] = elements[--nb_elements]; // Remplace l'élément supprimé par le dernier
+    void Remove(int element)
+    {
+        for (int i = 0; i < poslibre; i++)
+        {
+            if (tab[i] == element)
+            {
+                for (int j = i; j < poslibre - 1; j++)
+                {
+                    tab[j] = tab[j + 1];
+                }
+                poslibre--;
                 return;
             }
         }
-        cout << "L'élément " << element << " n'est pas dans l'ensemble." << endl;
     }
-
-    void copy(const Ensemble &other) {
-        empty();
-        for (int i = 0; i < other.nb_elements; ++i) {
-            add(other.elements[i]);
+    Ensemble Copy()
+    {
+        Ensemble newEnsemble(taille);
+        for (int i = 0; i < poslibre; i++)
+        {
+            newEnsemble.add(tab[i]);
         }
+        return newEnsemble;
     }
 
-    bool is_member(int element) {
-        for (int i = 0; i < nb_elements; ++i) {
-            if (elements[i] == element) {
+    bool isMember(int element)
+    {
+        for (int i = 0; i < poslibre; i++)
+        {
+            if (tab[i] == element)
                 return true;
-            }
         }
         return false;
     }
 
-    bool is_equal(const Ensemble &other) {
-        if (nb_elements != other.nb_elements) return false;
-
-        for (int i = 0; i < nb_elements; ++i) {
-            if (!other.is_member(elements[i])) {
+    bool isEqual(Ensemble &e)
+    {
+        if (poslibre != e.poslibre)
+            return false;
+        for (int i = 0; i < poslibre; i++)
+        {
+            if (!e.isMember(tab[i]))
                 return false;
-            }
         }
         return true;
     }
 
-    void print() {
-        cout << "Ensemble: { ";
-        for (int i = 0; i < nb_elements; ++i) {
-            cout << elements[i] << (i < nb_elements - 1 ? ", " : " ");
+    void print()
+    {
+        cout << "Ensemble : { ";
+        for (int i = 0; i < poslibre; i++)
+        {
+            cout << tab[i] << " ";
         }
         cout << "}" << endl;
     }
 };
 
-int main() {
-    Ensemble e;
-    e.add(10);
-    e.add(20);
-    e.add(30);
-    e.print();
+int main()
+{
+    Ensemble e1(5);
 
-    e.remove(20);
-    e.print();
+    if (e1.isEmpty())
+    {
+        cout << "L'ensemble est vide." << endl;
+    }
+    else
+    {
+        cout << "L'ensemble n'est pas vide." << endl;
+    }
 
-    e.add(40);
-    e.add(50);
-    e.print();
+    e1.add(1);
+    e1.add(2);
+    e1.add(3);
+    e1.add(4);
+    e1.print();
 
-    Ensemble e_copy;
-    e_copy.copy(e);
-    e_copy.print();
+    e1.Remove(2);
+    e1.print();
 
-    cout << "L'élément 30 est membre: " << (e.is_member(30) ? "Oui" : "Non") << endl;
-    cout << "L'élément 20 est membre: " << (e.is_member(20) ? "Oui" : "Non") << endl;
+    Ensemble e2 = e1.Copy();
+    e2.print();
 
-    Ensemble e2;
-    e2.add(10);
-    e2.add(30);
-    e2.add(40);
-    cout << "Les ensembles e et e2 sont égaux: " << (e.is_equal(e2) ? "Oui" : "Non") << endl;
+    if (e1.isMember(3))
+    {
+        cout << "L'element 3 est dans l'ensemble.\n";
+    }
 
+    else
+    {
+        cout <<"L'element 3 n'est pas dans l'ensemble.\n";
+    }
+
+    if (e1.isEqual(e2))
+    {
+        cout <<  "Les ensembles e1 et e2 sont egaux.\n";
+    }
+    else
+    {
+        cout << "Les ensembles e1 et e2 sont differents.\n";
+    }
     return 0;
 }
